853bf42aac085f29808878e17468046bb9820071
Oksana <kursorksu777@gmail.com>
2018-10-06 11:34:01 +0300

    module-10

--- module_10/hometask/js/script.js
+++ module_10/hometask/js/script.js
@@ -23,104 +23,130 @@
 const refs = {
   page: document.querySelector("body"),
   form: document.querySelector(".serch-form"),
-  searchUser : document.querySelector(".js-search"),
-  deleteUser : document.querySelector(".js-delete"),
-  putUser : document.querySelector(".js-put"),
-  addUser : document.querySelector(".js-add"),
-  allUsers :  document.querySelector(".js-all"),
-  listUsers : document.querySelector(".js-list tbody"),
-  idUser : document.querySelector(".js-id"),
-  nameUser : document.querySelector(".js-name"),
-  ageUser : document.querySelector(".js-age"),
-  popUpInner:  document.querySelector(".pop-up-inner"),
-  popUpClose:  document.querySelector(".pop-up-inner .btn-close"),
-  idWrap:  document.querySelector(".id-wrapper"),
-  newWrap:  document.querySelector(".new-user-wrapper"),
+  searchUser: document.querySelector(".js-search"),
+  deleteUser: document.querySelector(".js-delete"),
+  putUser: document.querySelector(".js-put:not(.new)"),
+  putUserNew: document.querySelector(".js-put.new"),
+  addUser: document.querySelector(".js-add"),
+  allUsers: document.querySelector(".js-all"),
+  listUsers: document.querySelector(".js-list tbody"),
+  idUser: document.querySelector(".js-id"),
+  nameUser: document.querySelector(".js-name"),
+  ageUser: document.querySelector(".js-age"),
+  popUpInner: document.querySelector(".pop-up-inner"),
+  popUpClose: document.querySelector(".pop-up-inner .btn-close"),
+  idWrap: document.querySelector(".id-wrapper"),
+  newWrap: document.querySelector(".new-user-wrapper"),
+  allButtons: document.querySelectorAll('.btn')
 }
 //=================================================================
 
 
-const showAllUsers = function(e) {
+const showAllUsers = function (e) {
   e.preventDefault();
   refs.form.reset();
   refs.page.classList.add('show-loader');
-  getAllUsers() 
+  getAllUsers()
     .then(items => {
       refs.page.classList.remove('show-loader');
       const murkup = createTable(items);
-      refs.listUsers.innerHTML = murkup; 
-  });
+      refs.listUsers.innerHTML = murkup;
+    });
   refs.form.reset();
 }
 
-const getUserById = function(e) {
+const getUserById = function (e) {
   e.preventDefault();
   let query = refs.idUser.value;
   try {
-    if(query === ''){
-      refs.idUser.setAttribute('placeholder',"Пожалуйста введите корректный id для продолжения"); 
+    if (query === '') {
+      refs.idUser.setAttribute('placeholder', "Пожалуйста введите корректный id для продолжения");
       return;
-    }else{
+    } else {
       refs.page.classList.add('show-loader');
       getUserId(query).then(item => refs.listUsers.innerHTML = murkupHTML(item));
       refs.page.classList.remove('show-loader');
       refs.form.reset();
+      refs.idUser.setAttribute('placeholder', "");
     }
   } catch (e) {
     console.log(`Ошибочка вышла : ${e}`);
   }
 }
 
-const putUserById = function(e) {
+const putUserById = function (e) {
   e.preventDefault();
-  let queryName = refs.nameUser.value;
-  let queryAge = refs.ageUser.value;
-  refs.idWrap.classList.add('hide');
-  refs.newWrap.classList.remove('hide');
-
+  let query = refs.idUser.value;
+  const name = prompt("Введите имя");
+  const age = prompt("Введите возраст");
+  console.log(age, name)
+  refs.page.classList.add('show-loader');
   try {
-    if(queryName === '' || queryAge === ''){
-      refs.idUser.setAttribute('placeholder',"Заполните поле"); 
+    if (!query) {
+      refs.idUser.setAttribute('placeholder', "Заполните поле");
       return;
-    }else{
+    } else {
       refs.page.classList.add('show-loader');
-      putUserId(queryName, queryAge );
+      putUserId(query, name, age)
       refs.page.classList.remove('show-loader');
       refs.form.reset();
+      refs.idUser.setAttribute('placeholder', '');
     }
   } catch (e) {
     console.log(`Ошибочка вышла : ${e}`);
   }
+  refs.newWrap.classList.add('hide');
 }
 
-const deleteUserById= function() {
+const deleteUserById = function () {
   let query = refs.idUser.value;
   try {
-    if(query === ''){
-      refs.idUser.setAttribute('placeholder',"Пожалуйста введите корректный id для продолжения"); 
+    if (query === '') {
+      refs.idUser.setAttribute('placeholder', "Пожалуйста введите корректный id для продолжения");
       return;
-    }else{
+    } else {
       refs.page.classList.add('show-loader');
       deleteUserId(query)
+        .then(item => {
+          if (item.error !== []) {
+            refs.popUpInner.querySelector('span').textContent = `Пользователь с идентификатором ${query} успешно удален`
+          } else {
+            refs.popUpInner.querySelector('span').textContent = `Пользователь с идентификатором ${query} не найден`
+          }
+        })
       refs.page.classList.remove('show-loader');
       refs.page.classList.add('del-success');
-      refs.popUpInner.querySelector('span').textContent = `Пользователь с идентификатором ${query} успешно удален`
     }
   } catch (e) {
     console.log(`Ошибочка вышла : ${e}`);
   }
 }
 
-const getAllUsers = () => 
+const addUser = function (e) {
+  e.preventDefault();
+  const name = prompt("Введите имя");
+  const age = prompt("Введите возраст");
+  console.log(age, name)
+  refs.page.classList.add('show-loader');
+  addNewUser(name, age)
+    .then(item => {
+      refs.popUpInner.querySelector('span').textContent = `Пользователь ${name} успешно создан`;
+      getAllUsers();
+    });
+  refs.page.classList.remove('show-loader');
+  refs.form.reset();
+}
+
+const getAllUsers = () =>
   fetch("https://test-users-api.herokuapp.com/users", {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
   })
   .then(response => {
-    if(response.ok) return response.json();
+    if (response.ok) return response.json();
     throw new Error(`Ошибочка вышла: ${response.statusText}`);
   })
   .then(data => {
@@ -128,18 +154,18 @@ const getAllUsers = () =>
   })
   .catch(error => {
     console.log(`Ошибочка вышла : ${error}`);
-});
+  });
 
-const getUserId = (query) => 
+const getUserId = (query) =>
   fetch(`https://test-users-api.herokuapp.com/users/${query}`, {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
   })
   .then(response => {
-    if(response.ok) return response.json();
+    if (response.ok) return response.json();
     throw new Error(`Ошибочка вышла: ${response.statusText}`);
   })
   .then(data => {
@@ -147,49 +173,51 @@ const getUserId = (query) =>
   })
   .catch(error => {
     console.log(`Ошибочка вышла : ${error}`);
-});
+  });
 
-const putUserId = (inputName, inputAge) => 
-  fetch(`https://test-users-api.herokuapp.com/users`, {
-  method: 'PUT',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
-  })
-  .then(response => {
-    if(response.ok) return response.json();
-    throw new Error(`Ошибочка вышла: ${response.statusText}`);
-  })
-  .then(data => {
-    return data.data;
-  })
-  .then(item => {
-    item.name = inputName;
-    item.age = inputAge;
-  })
-  .catch(error => {
-    console.log(`Ошибочка вышла : ${error}`);
-});
+const putUserId = (query, name, age) =>
+  fetch(`https://test-users-api.herokuapp.com/${query}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      name: `${name}`,
+      age: age
+    }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  });
 
-const deleteUserId = (query) => 
+const addNewUser = (name, age) =>
+  fetch('https://test-users-api.herokuapp.com/users', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: `${name}`,
+      age: age
+    }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  });
+const deleteUserId = (query) =>
   fetch(`https://test-users-api.herokuapp.com/users/${query}`, {
-  method: 'DELETE',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  }
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
   })
   .catch(error => {
     console.log(`Ошибочка вышла : ${error}`);
-});
+  });
 
 const createTable = (items) => {
-  return items.reduce((murkup,item) => murkup + murkupHTML(item), '');
+  return items.reduce((murkup, item) => murkup + murkupHTML(item), '');
 }
 
 const murkupHTML = (item) => {
-return `<tr>
+  return `<tr>
           <td class = "td td-id" width = "30%">${item.id}</td>
           <td class = "td td-name" width = "30%">${item.name}</td>
           <td class = "td td-age" width = "30%">${item.age}</td>
@@ -197,11 +225,13 @@ return `<tr>
 }
 
 const popUpClose = () => refs.page.classList.remove('del-success');
-
+// Эта функция должна удалять строку из таблички в интерфейсе
+//без перезагрузки страницы после того как сработает клик на кнопку Удалить
+//!Не работает
 const deletRow = (idCurrent) => {
-  refs.listUsers.querySelectorAll('.td-id')
+  refs.listUsers.querySelectorAll('.td-id');
   console.log(refs.listUsers.querySelectorAll('.td-id'));
-  refs.listUsers.querySelectorAll('.td-id')
+  refs.listUsers.querySelectorAll('.td-id');
   /*.forEach(item => {
     console.log(`${item.textContent == idCurrent}: ${item.textContent}`)
     if(item.textContent == idCurrent){
@@ -209,9 +239,19 @@ const deletRow = (idCurrent) => {
   }
 })*/
 };
+//Эта функция должна делать неактивными кнопки
+//Не работает
+const addDssabled = (button) => {
+
+  refs.allButtons.forEach(item => item.setAttribute('disabled', 'disabled'));
+  console.log(button)
+  button.removeAttribute('disabled');
+  console.log(refs.allButtons);
+}
+
 refs.popUpClose.addEventListener('click', popUpClose);
 refs.allUsers.addEventListener('click', showAllUsers);
 refs.searchUser.addEventListener('click', getUserById);
 refs.deleteUser.addEventListener('click', deleteUserById);
 refs.putUser.addEventListener('click', putUserById);
-//refs.addUser.addEventListener('click', addUser);
+refs.addUser.addEventListener('click', addUser);
